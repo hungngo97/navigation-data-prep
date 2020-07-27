0f99ca74c218b6381e6ce76acf16671abcca2efb
Hung Ngo <hung.ngovinh97@gmail.com>
2020-07-26 18:56:41 -0700

    Got IMU data

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from collections import defaultdict
 
 sensorsMap = {}
 payloads_metadata = {}
+raw_payloads = []
 
 with open('log.txt', 'r') as myfile:
     data = myfile.read()
     
-    payloads = data.split("\n\n\n")
-    print("Payload len: " , len(payloads))
+    raw_payloads = data.split("\n\n\n")
+    print("Payload len: " , len(raw_payloads))
 
     # Get sensor sampling rate
-    samplingRates = payloads[-1].split("\n")
+    samplingRates = raw_payloads[-1].split("\n")
 
     for samplingRate in samplingRates:
         words = samplingRate.split(" ")
@@ -36,8 +37,8 @@ with open('log.txt', 'r') as myfile:
     
     # Get time of each sensor sample
     sensor_counter = defaultdict(int)
-    for i in range(1, len(payloads) - 1):
-        payload = payloads[i]
+    for i in range(1, len(raw_payloads) - 1):
+        payload = raw_payloads[i]
         sensors_payload = payload.split("\n\n")
         
         payloads_metadata[i] = {}
@@ -100,5 +101,35 @@ with open('payload_metadata.json', 'w+') as f:
 
 
 
-    
+min_frequency = min(
+    sensorsMap['ACCL']['hz'],
+    sensorsMap['GYRO']['hz'],
+)
+
+with open('imu.csv', 'w') as imu_csv:
+    imu_writer = csv.writer(imu_csv, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for i in range(1, len(raw_payloads) - 1):
+        sensors_map = payloads_metadata[i]
+        accl_samples = sensors_map['ACCL']
+        gyro_samples = sensors_map['GYRO']
+        row_values = []
+
+        # TODO: Currently the GYRO frequency is 2x ACCL frequency, so index 1 of ACCL is GYRO of index 2
+        for accl_index in range(len(accl_samples)):
+            gyro_index = accl_index * 2
+            accl_value = accl_samples[accl_index]
+            gyro_value = gyro_samples[gyro_index]
+
+            row_values = [
+                min(accl_value['timestamp'], gyro_value['timestamp']),
+                gyro_value['x_value'], gyro_value['y_value'], gyro_value['z_value'],
+                accl_value['x_value'], accl_value['y_value'], accl_value['z_value']
+            ]
+            imu_writer.writerow(row_values)
+
+
+
+            
+        
 
